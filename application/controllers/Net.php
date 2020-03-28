1377183eb83c764b942e1610ecf7e2ed7738c31c
sadball4 <sadball4@hotmail.com>
2020-03-28 21:27:26 +0700

    update_construct

--- application/controllers/Net.php
+++ application/controllers/Net.php
@@ -2,9 +2,23 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Net extends CI_Controller {
+
+	function __construct() {
+        parent::__construct();
+
+		echo "you ";
+        
+    }
 	public function index()
 	{
 		echo "Thearayut Tancharoen";
+	}
+
+	function __destruct() {
+        parent::__construct();
+
+		echo " you ";
+        
     }
 
 }
