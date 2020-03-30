ca1a17f068d1c6c7e8817c2ee958109f6e03e1df
sadball4 <sadball4@hotmail.com>
2020-03-30 21:12:54 +0700

    update_link&image

---
+++ application/views/page1.php
@@ -0,0 +1,5 @@
+<?php
+echo "page1";
+?>
+<a href="<?php echo site_url('page1/page2')?>">To Page 2</a>
+<img src="<?php echo base_url('image/Logo_Lbtech.png');?>" width="40%">
---
+++ application/views/page2.php
@@ -0,0 +1,4 @@
+<?php
+echo "page2";
+?>
+<a href="<?php echo site_url('page1/index')?>">To Page 1</a>
